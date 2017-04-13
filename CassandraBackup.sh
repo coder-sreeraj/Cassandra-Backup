c9cff5cfc50d13c27f04da97dfb8120798c3f870
Sreeraju V <coder.sreeraj@gmail.com>
2017-04-13 11:30:20 +0530

    Update 2

--- CassandraBackup.sh
+++ CassandraBackup.sh
@@ -3,7 +3,7 @@
 
 DATE=`date +%Y-%m-%d`
 Backup_Location=/mnt/cassandra/backup
-DataDir__Location=/mnt/cassandra/data/*/*/snapshots
+DataDir_Location=/mnt/cassandra/data/*/*/snapshots
 IP=`ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/'`
 
 echo " For taking snapshot of specified keyspace,Enter Keyspace name: "
@@ -21,7 +21,7 @@ echo " Taking Backup of Table $table in Keyspace $KEYSPACE "
 nodetool flush
 nodetool snapshot -t $KEYSPACE-$TABLE-$DATE --table $TABLE $KEYSPACE
 echo "Snaphot tar archives stored in Location : $Backup_Location"
-tar czf $Backup_Location/$KEYSPACE-$DATE.tgz $DataDir__Location/$KEYSPACE-$TABLE-$DATE.tgz
+tar czf $Backup_Location/$KEYSPACE-$DATE.tgz $DataDir_Location/$KEYSPACE-$TABLE-$DATE.tgz
             ;;
 
         "Keyspace")
@@ -30,7 +30,7 @@ echo " Taking Backup of Keyspace $KEYSPACE "
 nodetool flush
 nodetool snapshot -t $KEYSPACE-$DATE $KEYSPACE
 echo "Snaphot tar archives stored in Location : $Backup_Location"
-tar czf $Backup_Location/$KEYSPACE-$DATE.tgz $DataDir__Location/$KEYSPACE-$DATE.tgz
+tar czf $Backup_Location/$KEYSPACE-$DATE.tgz $DataDir_Location/$KEYSPACE-$DATE.tgz
             ;;
 
         "CompleteKeyspace")
@@ -41,23 +41,23 @@ for Keyspace in `grep -v 'OpsCenter' /tmp/mykeyspace.cql`; do
     nodetool flush
     nodetool snapshot -t $KEYSPACE-$DATE $Keyspace
     echo "Snaphot tar archives stored in Location : $Backup_Location"
-    tar -cvzf $Backup_Location/$KEYSPACE-$DATE.tgz $DataDir__Location/$KEYSPACE-$DATE    
+    tar -cvzf $Backup_Location/$KEYSPACE-$DATE.tgz $DataDir_Location/$KEYSPACE-$DATE
             ;;
 
-        "S3 Upload")            
+        "S3 Upload")
 #Upload Archived to S3 Bucket
 echo "Enter S3 Bucket Name :"
 read S3Bucket
 echo "Uploading Snaphshot Archive file to S3 Bucket : $S3Bucket"
 aws s3 cp $Backup_Location/$KEYSPACE-$DATE.tgz  s3://$S3Bucket/
              ;;
-             
+
         "Clear Snapshot")
 #Clear all snapshot from Disk
 echo "Clearing all Snapshot taken in Disk"
-nodetool clearsnapshot 
+nodetool clearsnapshot
              ;;
-                 
+
         "Quit")
             break
-            ;;            
+            ;;
